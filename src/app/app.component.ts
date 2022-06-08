c1da318154abba84591780a3e7595e624f6b910e
Carlos Planchart <carlos_planchart@live.com>
2022-06-08 10:55:56 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'workDirectiveApp';
+  title = 'Register of Users';
+  message= '';
+  registered= false;
+  name='';
+  surname='';
+
+  registerUser(){
+    this.registered=true
+    this.message="User registered successfully."
+  }
 }
